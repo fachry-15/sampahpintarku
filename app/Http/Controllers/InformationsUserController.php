f5e7af114bf28e6da32b62aa557750c020168f4f
Hula-Hula <putriadeliakhairunnisa@gmail.com>
2025-03-28 11:27:06 +0700

    Add InformationsUser model, controller, and migration for user information storage

---
+++ app/Http/Controllers/InformationsUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InformationsUser;
+use Illuminate\Http\Request;
+
+class InformationsUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InformationsUser $informationsUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InformationsUser $informationsUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InformationsUser $informationsUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InformationsUser $informationsUser)
+    {
+        //
+    }
+}
